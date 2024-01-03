7f9f81b0452553efbbcefc50c576d0f3caec7673
Adrian Burke <114108495+AdrianBurke1@users.noreply.github.com>
2024-01-02 20:58:20 -0500

    update new

---
+++ capstone-project-front-end/src/component/Feed/ProfileCard.js
@@ -0,0 +1,14 @@
+// Profile.js
+import React from "react";
+import ProfileCard from "./ProfileCard"; // Assuming both are in the same directory
+
+const Profile = () => {
+  return (
+    <div className="profilefeed">
+    <img src="https://images.unsplash.com/photo-1507537297725-24a1c029d3ca?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"/>
+    <h2>Gavin</h2>
+  </div>
+  );
+};
+
+export default Profile;
--- capstone-project-front-end/src/component/RoadMap/Roadmap.jsx
+++ capstone-project-front-end/src/component/RoadMap/Roadmap.jsx
@@ -1,31 +1,45 @@
-import React from 'react';
-import './Roadmap.css';
+import React, { useEffect } from 'react';
 
 function Roadmap({ startingLocation, stepsInBetween, destination }) {
-  return (
-    <div className="amusement-park-map">
-      <div className="map-container">
-        <div className="attraction starting-location">Starting Point</div>
-        {startingLocation.map((location, index) => (
-          <div key={`start-${index}`} className="attraction start-marker">
-            {`S${index + 1}`}
-          </div>
-        ))}
+  useEffect(() => {
+    const initMap = () => {
 
-        {stepsInBetween.map((location, index) => (
-          <div key={`step-${index}`} className="attraction step-marker">
-            {`M${index + 1}`}
-          </div>
-        ))}
+      const centralParkCoordinates = {
+        lat: 40.7851,
+        lng: -73.9683,
+      };
 
-        {destination.map((location, index) => (
-          <div key={`dest-${index}`} className="attraction destination">
-            {`D${index + 1}`}
-          </div>
-        ))}
-      </div>
-    </div>
-  );
+      const map = new window.google.maps.Map(document.getElementById('map'), {
+        center: { lat: 0, lng: 0 }, // Center the map initially (adjust as needed)
+        zoom: 2, // Adjust the initial zoom level
+      });
+
+      const addMarker = (location, label) => {
+        new window.google.maps.Marker({
+          position: location,
+          map,
+          label,
+        });
+      };
+
+      startingLocation.forEach((location, index) => {
+        addMarker(location.coordinates, `S${index + 1}`);
+      });
+
+      stepsInBetween.forEach((location, index) => {
+        addMarker(location.coordinates, `M${index + 1}`);
+      });
+
+      destination.forEach((location, index) => {
+        addMarker(location.coordinates, `D${index + 1}`);
+      });
+    };
+
+    // Initialize the map when the component mounts
+    initMap();
+  }, [startingLocation, stepsInBetween, destination]);
+
+  return <div id="map" className="map"></div>;
 }
 
 export default Roadmap;
