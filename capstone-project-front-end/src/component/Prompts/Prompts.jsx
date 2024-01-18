1984f8374e23c03296ab67fcddf4b491cf28dfb4
paoAracena <Paolaaracena1233@gmail.com>
2024-01-18 09:47:23 -0500

    radio buttons

--- capstone-project-front-end/src/component/Prompts/Prompts.jsx
+++ capstone-project-front-end/src/component/Prompts/Prompts.jsx
@@ -13,6 +13,7 @@ function Prompts() {
   const [prompts, setPrompts] = useState([]);
   const [currentIndex, setCurrentIndex] = useState(0); // Initialize with 0
   const [todaysPrompt, setTodaysPrompt] = useState();
+  const [promptCategory, setPromptCategory] = useState('all');
 
   useEffect(() => {
     axios
@@ -49,6 +50,9 @@ function Prompts() {
     }
   }
 
+  function handleCategoryChange(category) {
+    setPromptCategory(category);
+}
   return (
     <div className='prompts'>
       {/* <div id="prev-prompts-btn">
@@ -70,17 +74,72 @@ function Prompts() {
 
       <div className='more-prompts'>
         <h1>More Prompts</h1>
-        <div className="scrolling-wrapper-flexbox">
-          {prompts.map((element) => (
-            <div key={element.id} className="daily-prompts">
-              <h3>{element.title}</h3>
-              <p className="elementpro">{element.prompt}</p>
-              <Link to={`/prompts/${element.id}`}>
-                <button className='start-promptne-btn' >Start</button>
-              </Link>
+        <div className="category-radio-btns">
+                <label>
+                    <input 
+                    type="radio"
+                    value="all"
+                    checked={promptCategory === 'all'}
+                    onChange={() => handleCategoryChange('all')}/>
+                    All
+                </label>
+
+                <label>
+                    <input 
+                    type="radio"
+                    value="Emotional Awareness"
+                    checked={promptCategory === "Emotional Awareness"}
+                    onChange={() => handleCategoryChange("Emotional Awareness")}/>
+                    Emotional Awareness
+                </label>
+
+                <label>
+                    <input 
+                    type="radio"
+                    value="Personal Development"
+                    checked={promptCategory === "Personal Development"}
+                    onChange={() => handleCategoryChange("Personal Development")}/>
+                    Personal Development
+                </label>
+
+                <label>
+                    <input 
+                    type="radio"
+                    value="Social Connection"
+                    checked={promptCategory === "Social Connection"}
+                    onChange={() => handleCategoryChange("Social Connection")}/>
+                    Social Connection
+                </label>
+
+                <label>
+                    <input 
+                    type="radio"
+                    value="Learning and Growth"
+                    checked={promptCategory === "Learning and Growth"}
+                    onChange={() => handleCategoryChange("Learning and Growth")}/>
+                    Learning and Growth
+                </label>
             </div>
-          ))}
+        <div className="scrolling-wrapper-flexbox">
+        {prompts
+                    .filter((element) => {
+                        if(promptCategory === 'all'){
+                            return true;
+                        } else {
+                            return element.type === promptCategory
+                        }
+                    })
+                    .map((element) => {
+                        return(
+                            <div key={element.id} className="daily-prompts">
+                            <h3>{element.title}</h3>
+                            <p className="elementpro">{element.prompt}</p>
+                            <Link to={`/prompts/${element.id}`}><button className='start-prompt-btn' >Start</button></Link>
+                            </div>
+                        )
+                    })}
         </div>
+       
       </div>
     </div>
   );
